92beb3882ddae76b71d364d1baa90f7d5d6b9718
Low Zhi Heng <lowzhiheng@gmail.com>
2019-01-21 23:13:58 +0800

    no message

---
+++ Assets/SUPERLASER/AsyncSceneLoader/AsyncSceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AsyncSceneLoader : MonoBehaviour
+{
+    [SerializeField] private Image loadingIndicator;
+    private static string targetScene;
+
+    private void Start()
+    {
+        StartCoroutine(LoadNextScene());
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static void AsyncLoadScene(string targetScene)
+    {
+        SceneManager.LoadScene("AsyncLoadingScene");
+        AsyncSceneLoader.targetScene = targetScene;
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        loadingIndicator.fillAmount = 0;
+        
+        Scene newScene = SceneManager.GetSceneByName(targetScene);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(targetScene);
+
+        while (!asyncOperation.isDone)
+        {
+            loadingIndicator.fillAmount = asyncOperation.progress * 1.111f;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/SUPERLASER/AsyncSceneLoader/AsyncSceneLoaderDemo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SUPERLASER
+{
+    public class AsyncSceneLoaderDemo : MonoBehaviour
+    {
+        public void LoadTargetScene()
+        {
+            AsyncSceneLoader.AsyncLoadScene("DemoScene-End");
+        }
+    }
+}
+
+
