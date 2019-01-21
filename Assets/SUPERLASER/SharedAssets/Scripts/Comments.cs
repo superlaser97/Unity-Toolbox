0155cd8b0da9dc8e86afe742a6076bce48a020e2
Low Zhi Heng <lowzhiheng@gmail.com>
2019-01-21 20:43:04 +0800

    no message

---
+++ Assets/SUPERLASER/SharedAssets/Scripts/Comments.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SUPERLASER
+{
+    public class Comments : MonoBehaviour
+    {
+        [SerializeField] [TextArea] private string Comment = "";
+    }
+}
---
+++ Assets/SUPERLASER/SharedAssets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SUPERLASER
+{
+    public class Timer
+    {
+        private float targetTime;
+
+        public void SetTimer(float duration)
+        {
+            targetTime = Time.time + duration;
+        }
+
+        public bool TimeIsUp => Time.time >= targetTime;
+        public float TimeLeft => targetTime - Time.time;
+    }
+
+}
+
